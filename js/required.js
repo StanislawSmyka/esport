b519d1f2f5ac75406d4d63677a77b076e90b0d78
StanislawSmyka <stachoo92@wp.pl>
2017-04-12 17:26:44 +0200

    nadchodzace wydarzenia na stronie glownej

--- js/required.js
+++ js/required.js
@@ -1,3 +1,12 @@
 $(function(){
        $("input").prop('required',true);
 });
+
+$(function(){
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    var $iframes = $(e.relatedTarget.hash).find('iframe');
+    $iframes.each(function(index, iframe){
+      $(iframe).attr("src", $(iframe).attr("src"));
+    });
+  });
+});
